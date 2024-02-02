c6ebec4a0b1f7ebe2f1ac40aecd1b54a9f347968
jf687 <jf687@cam.ac.uk>
2024-02-02 12:54:19 +0000

    updates in cutoff_curves and new runsim.R file

--- cutoff_curves.R
+++ cutoff_curves.R
@@ -3,7 +3,9 @@
 library(huge)
 library(ggplot2)
 library(flux)
+library(progress)
 source('simulations.R')
+
 
 # Some functions we need ----------------------------------
 
@@ -83,50 +85,57 @@ FPR = function(g, g.hat){
 #
 #
 
-perform_ROC_simulation = function(omega.true, n,list_hyper, list_init, N=100, include.glasso=T, include.ssl=T, include.VI=T, scale.data = T){
+perform_ROC_simulation = function(omega.true, n,list_hyper, list_init, N=100, include.glasso=T, include.ssl=T, scale.data = T){
   res=list()
   p=nrow(omega.true)
   sigma.true = solve(omega.true)
-  if(include.ssl){
-    n.ppi.thresh = 20 # Threshold the posterior inclusion probability to get different FDR (i.e. not thresholding matrix elements)
-    ppi.thresh = seq(0,1,n.ppi.thresh)
-    res$precisions.ssl = matrix(0,N,100)#n.ppi.thresh)
-    res$recalls.ssl = matrix(0,N,100)#n.ppi.thresh)
-    res$TPR.ssl = matrix(0,N,100)#n.ppi.thresh)
-    res$FPR.ssl = matrix(0,N,100)#n.ppi.thresh)
-  }
+  
   if(include.glasso){
     n.lambda = 20 # Varying the penalty parameter to get different FDRs
     res$precisions.glasso = matrix(0,N,n.lambda)
     res$recalls.glasso = matrix(0,N,n.lambda)
     res$TPR.glasso = matrix(0,N,n.lambda)
     res$FPR.glasso = matrix(0,N,n.lambda)
-  }
-
-  if(include.glasso){
-  for(i in 1:N){
-    y = mvtnorm::rmvnorm(n, rep(0, p), sigma.true)
-    if(scale.data){
-      y = scale(y)
-    }
-    res.glasso = huge(y, method = 'glasso', nlambda=n.lambda, verbose = F)
-    res.glasso.omegas = res.glasso$icov # A list of precision matrices
-    res$precisions.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) precision(abs(s)>1e-5, abs(omega.true)>1e-5)))
-    res$recalls.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) recall(abs(s)>1e-5, abs(omega.true)>1e-5)))
-    res$TPR.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) TPR(abs(s)>1e-5, abs(omega.true)>1e-5)))
-    res$FPR.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) FPR(abs(s)>1e-5, abs(omega.true)>1e-5)))
-    }
-  }
-  if(include.ssl){
-    ppi.thresh = 1:100 #c(100,90,80,70,60,50,40,30,20,10,9,8,7,6,5,4,3,2,1.9,1.8,1.7,1.6,1.5,1.4,1.3,1.2,1.1,1)
+    
+    
+    
     for(i in 1:N){
-      print(i)
-      #res.ssl.omegas <- list()
+      y = mvtnorm::rmvnorm(n, rep(0, p), sigma.true)
+      if(scale.data){
+        y = scale(y)
+      }
+      res.glasso = huge(y, method = 'glasso', nlambda=n.lambda, verbose = F)
+      res.glasso.omegas = res.glasso$icov # A list of precision matrices
+      res$precisions.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) precision(abs(s)>1e-5, abs(omega.true)>1e-5)))
+      res$recalls.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) recall(abs(s)>1e-5, abs(omega.true)>1e-5)))
+      res$TPR.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) TPR(abs(s)>1e-5, abs(omega.true)>1e-5)))
+      res$FPR.glasso[i,] = unlist(lapply(res.glasso.omegas, FUN = function(s) FPR(abs(s)>1e-5, abs(omega.true)>1e-5)))
+    }
+    
+    
+    # Average over all N simulations (i.e., find the average performance for each lambda/PPI)
+    # Glasso
+    res$mean.precisions.glasso = colMeans(res$precisions.glasso)
+    res$mean.recalls.glasso = colMeans(res$recalls.glasso)
+    res$mean.TPR.glasso = colMeans(res$TPR.glasso)
+    res$mean.FPR.glasso = colMeans(res$FPR.glasso) 
+  }
+  
+  
+  if(include.ssl){
+    n.ppi.thresh = 20 # Threshold the posterior inclusion probability to get different FDR (i.e. not thresholding matrix elements)
+    ppi.thresh = seq(0,1,n.ppi.thresh)
+    res$precisions.ssl = matrix(0,N,n.ppi.thresh)
+    res$recalls.ssl = matrix(0,N,n.ppi.thresh)
+    res$TPR.ssl = matrix(0,N,n.ppi.thresh)
+    res$FPR.ssl = matrix(0,N,n.ppi.thresh)
+    
+    pb <- progress_bar$new(format = "[:bar] :percent ETA: :eta", total = N)
+    
+    for(i in 1:N){
+      pb$tick()
       
-      
-      #GM <- GM_gen(n,p,list_hyper, list_init, thresh = x/n.ppi.thresh)
-      #adj_mat <- adj_gen(x/(n.ppi.thresh),p)
-      adj_mat <- adj_gen(0.08,p)
+      adj_mat <- adj_gen(0.02,p)
       new.omega.true <- prec_from_adj(adj_mat)
       new.sigma.true <- solve(new.omega.true)
       #new.omega.true <- GM$Omega
@@ -134,46 +143,40 @@ perform_ROC_simulation = function(omega.true, n,list_hyper, list_init, N=100, in
       if(scale.data){
         y = scale(y)
       }
-      for(x in ppi.thresh){
-      list_hyper$br <- x
+      
       res.ssl <- GM(y, list_hyper, list_init) # Must specify the hyper params as well
       res.ssl.omega <- res.ssl$Omega
+      res.ssl.m_delta <- res.ssl$m_delta
       #res.ssl.omegas[[x]] <- res.ssl.omega
       
       #for(x in 1:n.ppi.thresh){
-      res$precisions.ssl[i,x] <- precision(abs(res.ssl.omega)>1e-2, abs(new.omega.true)>1e-2)
-      res$recalls.ssl[i,x] <- recall(abs(res.ssl.omega)>1e-2, abs(new.omega.true)>1e-2)
-      res$TPR.ssl[i,x] <- TPR(abs(res.ssl.omega)>1e-2, abs(new.omega.true)>1e-2)
-      res$FPR.ssl[i,x] <- FPR(abs(res.ssl.omega)>1e-2, abs(new.omega.true)>1e-2)
+        #res$precisions.ssl[i,x] <- precision(abs(res.ssl$m_delta)>(x/(15*n.ppi.thresh)), abs(new.omega.true)>(x/(15*n.ppi.thresh)))
+        #res$recalls.ssl[i,x] <- recall(abs(res.ssl.m_delta)>(x/(15*n.ppi.thresh)), abs(new.omega.true)>(x/(15*n.ppi.thresh)))
+        #res$TPR.ssl[i,x] <- TPR(abs(res.ssl$m_delta)>(x/(15*n.ppi.thresh)), abs(new.omega.true)>(x/(15*n.ppi.thresh)))
+        #res$FPR.ssl[i,x] <- FPR(abs(res.ssl$m_delta)>(x/(15*n.ppi.thresh)), abs(new.omega.true)>(x/(15*n.ppi.thresh)))
+      #}
       
-    #res.ssl.omegas <- lapply(ppi.thresh, FUN = function(s) omegas.list$PPI < s) # A list of prec.matrices. Also probably not how you get the PPI, fix this
-    #res$precisions.ssl[i,x] = unlist(lapply(res.ssl.omegas, FUN = function(s) precision(abs(s)>1e-5, abs(omega.true)>1e-5)))
-    #res$recalls.ssl[i,x] = unlist(lapply(res.ssl.omegas, FUN = function(s) recall(abs(s)>1e-5, abs(omega.true)>1e-5)))
-    #res$TPR.ssl[i,x] = unlist(lapply(res.ssl.omegas, FUN = function(s) TPR(abs(s)>1e-5, abs(omega.true)>1e-5)))
-    #res$FPR.ssl[i,x] = unlist(lapply(res.ssl.omegas, FUN = function(s) FPR(abs(s)>1e-5, abs(omega.true)>1e-5)))
+      for(x in 1:n.ppi.thresh){
+        res$precisions.ssl[i,x] <- precision(abs(res.ssl.omega)>(x/(10*n.ppi.thresh)), abs(new.omega.true)>(x/(10*n.ppi.thresh)))
+        res$recalls.ssl[i,x] <- recall(abs(res.ssl.omega)>(x/(10*n.ppi.thresh)), abs(new.omega.true)>(x/(10*n.ppi.thresh)))
+        res$TPR.ssl[i,x] <- TPR(abs(res.ssl.omega)>(x/(10*n.ppi.thresh)), abs(new.omega.true)>(x/(10*n.ppi.thresh)))
+        res$FPR.ssl[i,x] <- FPR(abs(res.ssl.omega)>(x/(10*n.ppi.thresh)), abs(new.omega.true)>(x/(10*n.ppi.thresh)))
       }
+      
     }
-  
-  }
-
-  
-  # Average over all N simulations (i.e., find the average performance for each lambda/PPI)
-  # Glasso
-  if(include.glasso){
-    res$mean.precisions.glasso = colMeans(res$precisions.glasso)
-    res$mean.recalls.glasso = colMeans(res$recalls.glasso)
-    res$mean.TPR.glasso = colMeans(res$TPR.glasso)
-    res$mean.FPR.glasso = colMeans(res$FPR.glasso) 
-  }
-  # Spike-and-slab
-  if(include.ssl){
+    
+    
+    
     res$mean.precisions.ssl = colMeans(res$precisions.ssl)
     res$mean.recalls.ssl = colMeans(res$recalls.ssl)
     res$mean.TPR.ssl = colMeans(res$TPR.ssl)
     res$mean.FPR.ssl = colMeans(res$FPR.ssl) 
+  
   }
+  
   return(res)
 }
+
 
 
 plot_ROC = function(sim.obj, include.glasso=T, include.ssl=T, cutoff=NULL){
@@ -283,28 +286,6 @@ get_PRC_AUC = function(sim.obj, method='Glasso', cutoff=NULL){
   res= list(AUC=flux::auc(x, y), cutoff=cutoff)
   return(res)
 }
-
-
-# Test the code, for only the graphical lasso ------------------------------------
-
-n=40
-p=20
-N=10
-ggm.sf = GGM_gen(n,p)
-omega.true = ggm.sf$omega.true
-res.sim = perform_ROC_simulation(omega.true, n, list_hyper, list_init, include.ssl=T,N)
-# Plot ROC curve
-cutoff <- get_AUC(res.sim, method = 'Glasso')
-cutoff <- cutoff$cutoff
-plot_ROC(res.sim, include.ssl = T)
-plot_PRC(res.sim, include.ssl = T)
-
-# Get cut-off AUC
-get_AUC(res.sim, method='Glasso')
-get_PRC_AUC(res.sim, method='Glasso')
-
-#get_AUC(res.sim, method='SSL')
-#get_PRC_AUC(res.sim, method='SSL')
 
 
 
---
+++ runsim.R
@@ -0,0 +1,42 @@
+source('cutoff_curves.R')
+source('GM.R')
+source('fun_GM.R')
+source('fun_utils.R')
+
+# Test the code, for only the graphical lasso ------------------------------------
+
+seed <- 123; 
+set.seed(seed)
+n <- 200
+p <- 100
+N <- 100
+Y <- matrix(rnorm(n*p), nrow = n, ncol = p)
+list_hyper <- list(lambda = 2,                   
+                   v0 = 100,                  
+                   v1 = 0.5,                   
+                   a = 2,                   
+                   b = 2,                   
+                   ar = 1,                   
+                   br = p)
+list_init <- list(a_rho = 1,                  
+                  b_rho = 1,                  
+                  a_tau = 1,                  
+                  b_tau = 1)
+
+
+
+ggm.sf = GGM_gen(n,p)
+omega.true = ggm.sf$omega.true
+res.sim <- perform_ROC_simulation(omega.true, n, list_hyper, list_init, include.ssl=T,N)
+# Plot ROC curve
+cutoff <- get_AUC(res.sim, method = 'Glasso')
+cutoff <- cutoff$cutoff
+plot_ROC(res.sim, include.ssl = T)
+plot_PRC(res.sim, include.ssl = T)
+
+# Get cut-off AUC
+get_AUC(res.sim, method='Glasso')
+get_PRC_AUC(res.sim, method='Glasso')
+
+#get_AUC(res.sim, method='SSL')
+#get_PRC_AUC(res.sim, method='SSL')
