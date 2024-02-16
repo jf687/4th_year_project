f713fc7ea2885f62707bf63bb1e62ec597a092c8
Xiaoyue <xiaoyue.xi94@gmail.com>
2024-02-16 23:22:56 +0800

    bugfix

--- example_search.R
+++ example_search.R
@@ -1,39 +1,23 @@
 if(!require(remotes)) install.packages("remotes")
 remotes::install_github("XiaoyueXI/navigm")
 library(navigm)
+library(huge)
 
-seed <- 123; set.seed(seed)
-N <- 100; P <- 50;
-Y <- matrix(rnorm(N*P), nrow = N, ncol = P)
-
-# estimate a precision matrix based on Y 
-lambda <- 2
-<<<<<<< HEAD
-v0_v <- s0_v <- seq(1e-2, 1, length.out = 16)
-v1 <- s1 <- 100
-P <- ncol(Y)# NOT RUN {
 #generate data
-L = huge.generator(d = 20, graph="hub")
-out.mb = huge(L$data)
-out.ct = huge(L$data, method = "ct")
+
+set.seed(123)
+L = huge.generator(d = 50, graph="hub")
+# L = huge.generator(d = 50, graph="scale-free")
+par(mfrow = c(1,2))
+image(L$omega)
+
 out.glasso = huge(L$data, method = "glasso")
-
-#model selection using ric
-out.select = huge.select(out.mb)
-plot(out.select)
-
-#model selection using stars
-#out.select = huge.select(out.ct, criterion = "stars", stars.thresh = 0.05,rep.num=10)
-#plot(out.select)
-
-#model selection using ebic
 out.select = huge.select(out.glasso,criterion = "ebic")
-plot(out.select)
-=======
+
+# 
 v0_v <- seq(1e-2, 1, length.out = 16)
 v1 <- 100
-P <- ncol(V)
->>>>>>> ab795f0b846821b23a836809263fc49ad232f80b
+P <- ncol(L$data)
 
 #
 list_hyper <- list(
@@ -45,20 +29,33 @@ list_hyper <- list(
   a_rho = 1,
   b_rho = P)
 
-
 list_init <-
   list(
     alpha_tau = 1,
     beta_tau = 1,
     alpha_rho = 1,
-    beta_rho = P-1
+    beta_rho = P
   )
 
-res1 <- navigm(Y, method = 'GM', 
-                     version = 1,
-                     list_hyper = list_hyper,
-                     list_init = list_init, 
-                     tol = 1e-3,
-                     maxit = 1e5)
+res1 <- navigm(L$data, 
+               method = 'GM', 
+               version = 1,
+               list_hyper = list_hyper,
+               list_init = list_init, 
+               tol = 1e-3,
+               maxit = 1e5,
+               full_output = T,
+               debug = T)
 
-res1$estimates$m_delta
+res1$model_criterion
+
+pred <- prediction(res1$estimates$m_delta[bool_up], as.matrix(L$theta)[bool_up])
+perf <- performance(pred,"tpr","fpr")
+plot(perf)
+pred <- prediction(out.select$opt.icov[bool_up], as.matrix(L$theta)[bool_up])
+perf <- performance(pred,"tpr","fpr")
+plot(perf, add = T ,col = "red")
+
+# image(res1$estimates$m_delta > 0.5)
+# image(out.select$opt.icov)
+
