a984264576517daf6cf5592f5958918111d0e6c3
jf687 <jf687@cam.ac.uk>
2024-02-22 13:18:35 +0000

    small meeting updates

--- Sims/GM_ROC_sim.R
+++ Sims/GM_ROC_sim.R
@@ -101,7 +101,7 @@ v0_grid <- function(list_hyper, list_init, v0_list = list(0.05, 0.1, 0.5, 1, 5, 
   return(op)
 }
 
-GM_ROC_sim <- function(n = 100, p = 50, plot = F){
+GM_ROC_sim <- function(n = 400, p = 50, plot = F){
   
   # create simulated data Y
   
@@ -119,7 +119,7 @@ GM_ROC_sim <- function(n = 100, p = 50, plot = F){
   plot(out.select)
   
   #set up grid of v0
-  v0_v <- seq(1e-2, 1, length.out = 32)
+  v0_v <- seq(1e-6, 9e-2, length.out = 32)
   v1 <- 100
   
   #initialise list parameters
@@ -130,7 +130,7 @@ GM_ROC_sim <- function(n = 100, p = 50, plot = F){
     a_tau = 2,
     b_tau = 2,
     a_rho = 1,
-    b_rho = p)
+    b_rho = p/3)
   
   
   list_init <-
@@ -160,15 +160,19 @@ GM_ROC_sim <- function(n = 100, p = 50, plot = F){
   perf <- ROCR::performance(pred, measure = "tpr", x.measure = "fpr")
   
   auc_value <- ROCR::performance(pred, "auc")@y.values[[1]]
+  
+  ## THRESHOLD ON 0.5
+  
   prec_value <- ROCR::performance(pred, "prec")@y.values[[1]][613]
-  browser()
-  rec_vaue <- ROCR::performance(pred, "recall")@y.values[[1]][613]
+
+  rec_value <- ROCR::performance(pred, "rec")@y.values[[1]][613]
   
   if(plot){
-    plot(v0_v[2:16],res1$model_criterion$value[2:16], xlab = 'value of spike variance v0', ylab = 'AIC', type = 'l')
+    plot(v0_v[2:32],res1$model_criterion$value[2:32], xlab = 'value of spike variance v0', ylab = 'AIC', type = 'l')
     plot(perf, main = '', xlim = c(0, 1), ylim = c(0, 1), lwd = 2, colorize = TRUE)
   }
   # an example of cutoffs 
+  hist(res1$estimates$m_delta) 
   browser()
   cm <- confusion.matrix(labels[bool_up], predictions[bool_up] > 0.5)
   return(list(n=n,p=p,auc_value = auc_value, prec_value = prec_value, rec_value = rec_value, AIC = res1$model_criterion$value, 
